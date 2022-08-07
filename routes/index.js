9e153e94e32248c50359119ffc86d67cb705b13c
窦一鸣 <douyiming@192.168.0.102>
2022-08-07 21:04:24 +0800

    finish

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,51 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+
+const users = {
+  a: {
+    name: 'A',
+    balance: 1000,
+  },
+  b: {
+    name: 'B',
+    balance: 1000,
+  },
+  other: {
+    name: 'other',
+    balance: 1000,
+  },
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('welcome');
-});
+router.get('/', (req, res, next) => {
+  const cookies = req.cookies
+  const username = cookies.sessionId
+  const params = username ? users[username] : { name: '', balance: '' }
+  res.render('index', params)
+})
 
-module.exports = router;
+router.get('/login', (req, res, next) => {
+  const username = req.query.username
+  const params = users[username]
+  res.set('Set-Cookie', `sessionId=${username}`) // ;SameSite=None
+  res.render('index', params)
+})
+
+router.post('/transfer', (req, res, next) => {
+  const cookies = req.cookies
+  const username = cookies.sessionId
+  const toUserName = req.body.username
+  const amount = Number(req.body.amount)
+  const currentUser = users[username]
+  const toUser = users[toUserName]
+  currentUser.balance = currentUser.balance - amount
+  toUser.balance = toUser.balance + amount
+
+  res.json({ code: 0 })
+})
+
+router.get('/search', (req, res, next) => {
+  const query = req.query
+  res.render('search-result', { searchKey: query.q })
+})
+module.exports = router
--- views/src/layouts/default.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <BaseHeader />
-  <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
-    <RouterView />
-    <Footer />
-  </main>
-</template>
